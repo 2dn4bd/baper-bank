dc6d7a6bf9d5989919f9bb92cd27f86f6b344bd9
2dn4bd <sohelmahmud047@gmail.com>
2023-08-15 19:17:11 +0600

    complete

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,6 @@
 // dashBorad
 // deposit 
-document.getElementById('deposit-btn').addEventListener('click',function(){
+document.getElementById('deposit-btn').addEventListener('click', function(){
     const depositField = document.getElementById('deposit-input');
     const depositValue = depositField.value;
     const depoValueNumber = parseFloat(depositValue);
@@ -8,26 +8,33 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     const depoTotal = depoAmount.innerText;
     const depoTotalNUmber = parseFloat(depoTotal)
     const currentTotal = depoTotalNUmber + depoValueNumber;
-    depositField.value = '';
     depoAmount.innerText = currentTotal;
+//balance
+// deposit
+    const balanceTotal = document.getElementById('balance-amount');
+    const balanceTotalStr = balanceTotal.innerText;
+    const mainBalanceTotal = parseFloat(balanceTotalStr);
+    const currentBalance = mainBalanceTotal + depoValueNumber;
+    balanceTotal.innerText = currentBalance;
+    depositField.value = '';
 })
 
-//balance
+//withdraw
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const withdrawField = document.getElementById('withdraw-input');
+    const withdrawValue = withdrawField.value;
+    const withdrawValueNumber = parseFloat(withdrawValue);
+    const withAmount = document.getElementById('withdraw-amount');
+    const withTotal = withAmount.innerText;
+    const withTotalNUmber = parseFloat(withTotal);
+    const currentTotal = withTotalNUmber + withdrawValueNumber;
+    withAmount.innerText = currentTotal;
+    withdrawField.value = '';
+// balance
+//withdraw
 const balanceTotal = document.getElementById('balance-amount');
 const balanceTotalStr = balanceTotal.innerText;
 const mainBalanceTotal = parseFloat(balanceTotalStr);
-const currentBalance = mainBalanceTotal + depoValueNumber;
+const currentBalance = mainBalanceTotal - withdrawValueNumber;
 balanceTotal.innerText = currentBalance;
-
-//withdraw
-// document.getElementById('withdraw-btn').addEventListener('click', function(){
-//     const withdrawField = document.getElementById('withdraw-input');
-//     const withdrawValue = withdrawField.value;
-//     const withdrawValueNumber = parseFloat(withdrawValue);
-//     const withAmount = document.getElementById('withdraw-amount');
-//     const withTotal = withAmount.innerText;
-//     const withTotalNUmber = parseFloat(withTotal);
-//     const currentTotal = withTotalNUmber + withdrawValueNumber;
-//     withdrawField.value = '';
-//     withAmount.innerText = currentTotal;
-// })
+})
