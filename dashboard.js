787d05d708f357e2bf8155d7cf229876458a73c8
2dn4bd <sohelmahmud047@gmail.com>
2023-08-15 14:55:57 +0600

    added some js

--- dashboard.js
+++ dashboard.js
@@ -3,16 +3,27 @@
 document.getElementById('deposit-btn').addEventListener('click',function(){
     const depositField = document.getElementById('deposit-input');
     const depositValue = depositField.value;
+    const depoValueNumber = parseFloat(depositValue);
     const depoAmount = document.getElementById('deposit-amount');
-    depoAmount.innerText = (depositValue);
+    const depoTotal = depoAmount.innerText;
+    const depoTotalNUmber = parseFloat(depoTotal)
+    const currentTotal = depoTotalNUmber + depoValueNumber;
     depositField.value = '';
+    depoAmount.innerText = currentTotal;
 })
 
 //withdraw
 document.getElementById('withdraw-btn').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-input');
     const withdrawValue = withdrawField.value;
+    const withdrawValueNumber = parseFloat(withdrawValue);
     const withAmount = document.getElementById('withdraw-amount');
-    withAmount.innerText = (withdrawValue);
+    const withTotal = withAmount.innerText;
+    const withTotalNUmber = parseFloat(withTotal);
+    const currentTotal = withTotalNUmber + withdrawValueNumber;
     withdrawField.value = '';
+    withAmount.innerText = currentTotal;
+    
+//balance
+
 })
